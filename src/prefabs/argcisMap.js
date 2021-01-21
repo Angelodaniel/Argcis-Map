424ae449b7a8c07227fc304c1516994fa709ed6b
Angelo de Voer <angelo_de_voer@hotmail.com>
2021-01-21 08:56:45 +0100

    feat: argcis map

---
+++ src/prefabs/argcisMap.js
@@ -0,0 +1,12 @@
+(() => ({
+  name: 'Argcis Map',
+  icon: 'RowColumnIcon',
+  category: 'LAYOUT',
+  structure: [
+    {
+      name: 'Arcgis Map',
+      options: [],
+      descendants: [],
+    },
+  ],
+}))();
